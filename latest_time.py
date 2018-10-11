704833e961030418b8a5f39e57d9816c53dc0b3a
Abbas Yadollahi <abbasyadollahi@outlook.com>
2018-10-11 02:37:17 -0400

    Couple google questions

---
+++ latest_time.py
@@ -0,0 +1,31 @@
+from itertools import product
+class Solution(object):
+    def latestTime(self, time):
+        """
+        :type time: str
+        :rtype: str
+        """
+
+        digits = set(time.replace(':', ''))
+        hh, mm = time[:2], time[3:]
+
+        if len(digits) == 1:
+            return time
+
+        permutations = [''.join(num) for num in product(digits, repeat=2)]
+
+        mmm = max([num for num in permutations if num < mm]) if mm != '00' else max([num for num in permutations if num < '60'])
+        hhh = hh
+        if mmm >= mm:
+            hhh = max([num for num in permutations if num < hh]) if hh != '00' else max([num for num in permutations if num < '24'])
+
+        return hhh + ':' + mmm
+
+
+sol = Solution()
+print(sol.latestTime('11:01'))
+print(sol.latestTime('10:00'))
+print(sol.latestTime('22:22'))
+print(sol.latestTime('22:21'))
+print(sol.latestTime('00:01'))
+print(sol.latestTime('23:59'))
---
+++ rose_bushes.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def latestBushGroup(self, a, k, m):
+        """
+        :type a: list
+        :type k: int
+        :type m: int
+        :rtype: int
+        """
+
+        a_len = len(a)
+        days = [1] * a_len
+
+        for day, i in enumerate(a[-1:k*m-a_len-1:-1]):
+            days[i-1] = 0
+
+            k_count = 0
+            m_count = 0
+
+            for rose in days:
+                if rose == 1:
+                    k_count += 1
+                else:
+                    if k_count >= k:
+                        m_count += 1
+                    k_count = 0
+
+            if k_count >= k:
+                m_count += 1
+
+            if m_count >= m:
+                return a_len - day - 1
+
+        return -1
+
+sol = Solution()
+
+print(sol.latestBushGroup([1,4,3,2,5], 1, 3))
+print(sol.latestBushGroup([1,2,7,6,4,3,5], 2, 2))
+print(sol.latestBushGroup([1,2,3,5,6,7,4], 3, 2))
