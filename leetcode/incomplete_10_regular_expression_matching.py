af7ed4baa7ebb529484f8ba392cc76e5806712f8
Abbas Yadollahi <abbasyadollahi@outlook.com>
2023-05-24 13:28:56 -0700

    Incomplete problems (must get good)

---
+++ leetcode/incomplete_10_regular_expression_matching.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/regular-expression-matching/
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        i_s = 0
+        i_p = 0
+
+        pp = p + ' '
+        continuous = ''
+        regex_groups = []
+        while i_p < len(p):
+            if pp[i_p+1] == '*':
+                if continuous:
+                    regex_groups.append([continuous, False])
+                    continuous = ''
+                regex_groups.append([p[i_p], True])
+                i_p += 2
+            else:
+                continuous += p[i_p]
+                i_p += 1
+
+        if continuous:
+            regex_groups.append([continuous, False])
---
+++ leetcode/incomplete_787_cheapest_flights_within_k_stops.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
+import heapq
+from collections import defaultdict
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: list[list[int]], src: int, dst: int, k: int) -> int:
+        connections = defaultdict(list)
+        for source, destination, price in flights:
+            connections[source].append((destination, price))
+
+        prices = [float('inf')] * n
+        prices[src] = 0
+
+        queue = [(src, 0)]
+        for _ in range(k + 1):
+            next_queue = []
+            while queue:
+                source, source_price = queue.pop()
+                for destination, destination_price in connections[source]:
+                    if source_price + destination_price < prices[destination]:
+                        prices[destination] = source_price + destination_price
+                        next_queue.append((destination, prices[destination]))
+            queue = next_queue
+
+        if prices[dst] < float('inf'):
+            return prices[dst]
+        else:
+            return -1
