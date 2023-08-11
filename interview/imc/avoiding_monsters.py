0425b1e90c9ad8efc3a53163004dd07ca47ca528
Abbas Yadollahi <abbasyadollahi@outlook.com>
2023-08-11 14:38:18 -0400

    RIP to Saba's IMC assessment

---
+++ interview/imc/avoiding_monsters.py
@@ -0,0 +1,90 @@
+def avoidMonsters(
+    m: int,
+    n: int,
+    start: tuple[int, int],
+    end: tuple[int, int],
+    monsters: list[tuple[int, int]],
+) -> int:
+    grid = [
+        [
+            min(
+                distance(i, j, monster_i, monster_j)
+                for monster_i, monster_j in monsters
+            )
+            for j in range(n)
+        ]
+        for i in range(m)
+    ]
+
+    return max(traverse(grid, *start, end, float("inf"), set()))
+
+
+def traverse(
+    grid: list[list[int]],
+    i: int,
+    j: int,
+    end: tuple[int, int],
+    minimum: int,
+    visited: set[tuple[int, int]],
+) -> list[int]:
+    if (i, j) in visited or i not in range(len(grid)) or j not in range(len(grid[0])):
+        return []
+
+    visited.add((i, j))
+
+    if (i, j) == end:
+        return [min(minimum, grid[i][j])]
+
+    options = []
+    if i - 1 >= 0 and (i - 1, j) not in visited:
+        options.append((i - 1, j))
+    if i + 1 < len(grid) and (i + 1, j) not in visited:
+        options.append((i + 1, j))
+    if j - 1 >= 0 and (i, j - 1) not in visited:
+        options.append((i, j - 1))
+    if j + 1 < len(grid[0]) and (i, j + 1) not in visited:
+        options.append((i, j + 1))
+
+    distances = []
+    maximum = max((grid[ii][jj] for ii, jj in options), default=None)
+    for ii, jj in options:
+        if grid[ii][jj] == maximum:
+            distances.extend(
+                traverse(grid, ii, jj, end, min(minimum, maximum), visited)
+            )
+            visited.remove((ii, jj))
+
+    return distances
+
+
+def distance(x1: int, y1: int, x2: int, y2: int) -> int:
+    return abs(x2 - x1) + abs(y2 - y1)
+
+
+print(
+    avoidMonsters(
+        5,
+        5,
+        (0, 0),
+        (3, 3),
+        [
+            (0, 3),
+            (1, 4),
+        ],
+    )
+)
+
+print(
+    avoidMonsters(
+        5,
+        5,
+        (0, 0),
+        (4, 4),
+        [
+            (2, 3),
+            (2, 4),
+            (3, 2),
+            (4, 2),
+        ],
+    )
+)
---
+++ interview/imc/real_estate_analysis.py
@@ -0,0 +1,128 @@
+import bisect
+import statistics
+from collections import defaultdict
+from dataclasses import dataclass
+
+
+@dataclass
+class Analysis:
+    comparisons: list[int]
+    average: float | None
+    standard_deviation: float | None
+    absolute_difference: float | None
+    outlier: bool
+
+
+def findValuation(reqArea: int, areas: list[int], prices: list[int]) -> int:
+    candidates = generateCandidates(areas, prices)
+
+    if not candidates:
+        return 1000
+
+    if len(candidates) == 1:
+        (_, price), _ = candidates.popitem()
+        return formalize_price(price)
+
+    comparisons_by_area = defaultdict(list)
+    for area, price in candidates:
+        comparisons_by_area[area].append(price)
+
+    if reqArea in comparisons_by_area:
+        return formalize_price(statistics.fmean(comparisons_by_area[reqArea]))
+
+    areas = sorted(comparisons_by_area.keys())
+
+    if reqArea < areas[0]:
+        i = 0
+    elif reqArea > areas[-1]:
+        i = -2
+    else:
+        i = bisect.bisect_right(areas, reqArea) - 1
+
+    x1, y1 = areas[i], statistics.fmean(comparisons_by_area[areas[i]])
+    x2, y2 = areas[i+1], statistics.fmean(comparisons_by_area[areas[i+1]])
+
+    return formalize_price(extrapolate(reqArea, x1, y1, x2, y2))
+
+
+def generateCandidates(areas: list[int], prices: list[int]) -> dict[tuple[int, int], Analysis]:
+    """
+    Generate all candidates and remove the outliers.
+    """
+
+    houses = list(zip(areas, prices))
+    candidates = {
+        house: Analysis(
+            comparisons=[],
+            average=None,
+            standard_deviation=None,
+            absolute_difference=None,
+            outlier=False,
+        )
+        for house in houses
+    }
+
+    for i, (area, price) in enumerate(houses):
+        for j, (other_area, other_price) in enumerate(houses):
+            if area == other_area and i != j:
+                candidates[(area, price)].comparisons.append(other_price)
+
+    for (area, price), analysis in candidates.items():
+        if not analysis.comparisons:
+            continue
+
+        analysis.average = statistics.fmean(analysis.comparisons)
+        analysis.standard_deviation = statistics.pstdev(analysis.comparisons) if len(analysis.comparisons) > 1 else 0
+        analysis.absolute_difference = abs(price - analysis.average)
+        analysis.outlier = analysis.absolute_difference > 3 * analysis.standard_deviation
+
+    candidates = {
+        house: analysis
+        for house, analysis in candidates.items()
+        if not analysis.outlier
+    }
+
+    return candidates
+
+
+def extrapolate(x: int, x1: int, y1: int, x2: int, y2: int) -> float:
+    """
+    Extrapolate the price of a house given its area.
+    """
+
+    return y1 + (x - x1) * (y2 - y1) / (x2 - x1)
+
+
+def formalize_price(price: float) -> int:
+    """
+    Round to nearest integer and limit to 10^3 and 10^6.
+    """
+
+    price = max(price, 10 ** 3)
+    price = min(price, 10 ** 6)
+    return round(price)
+
+
+print(
+    findValuation(
+        1500,
+        [1200, 1300, 1200, 1300, 1200, 2000],
+        [12000, 24000, 14000, 22000, 13000, 30000],
+    )
+)
+
+print(
+    findValuation(
+        1200,
+        [1500, 500, 1000, 2000, 2500],
+        [30000, 10000, 20000, 40000, 50000],
+    )
+)
+
+print(
+    findValuation(
+        2500,
+        [1200, 1200, 1200, 2000],
+        [15000, 11000, 17000, 25000],
+    )
+)
